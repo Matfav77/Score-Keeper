e5b21ab65a77a65d667139739e1b6ce2d1f5c94d
Mathieu Favre <103691353+Matfav77@users.noreply.github.com>
2022-11-03 18:49:15 +0100

    Added tracking of players added, and tracking of scores, little styling

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
-const newPlayerForm = document.querySelector("#new-player-form");
+const newPlayerForm = document.querySelector(".new-player-form");
+const submitBtn = document.querySelector(".new-player-form__btn");
+const nameInput = document.querySelector("#new-player-form__input");
+
+const scoreTable = document.querySelector(".score-table");
+const playerBtnsDisplay = document.querySelector(".player-btns");
+
+const resetBtn = document.querySelector(".reset-btn");
+
+const maxNumbPlayers = 8;
+const playerList = [];
 
 newPlayerForm.addEventListener('submit', function (e) {
     e.preventDefault();
+    if (playerList.length < maxNumbPlayers) {
+        let playerInput = newPlayerForm.elements["playerName"];
+        newPlayerDisplay = document.createElement("span");
+        newPlayerDisplay.innerText = 0;
+        newPlayerDisplay.classList.add("score-display");
+        scoreTable.append(newPlayerDisplay);
+        let newScoreBtn = document.createElement("button");
+        newScoreBtn.classList.add("button");
+        newScoreBtn.innerText = `+1 ${playerInput.value}`;
+        playerBtnsDisplay.append(newScoreBtn);
+        let newPlayer = {
+            score: 0,
+            name: playerInput.value,
+            display: newPlayerDisplay,
+            button: newScoreBtn
+        }
+        playerList.push(newPlayer);
+        playerInput.value = "";
+        if (playerList.length === maxNumbPlayers) {
+            document.querySelector(".new-player-form__label").innerText = "Max number of players reached";
+            submitBtn.classList.add("hide");
+            nameInput.classList.add("hide");
+        }
+    }
+})
+
+// for (i = 0; i < playerList.length; i++) {
+//     playerList[i].button.addEventListener('click', function () {
+//         playerList[i].score++;
+//         this.innerText = playerList[i].score;
+//     })
+// }
+
+playerBtnsDisplay.addEventListener('click', function (e) {
+    for (i = 0; i < playerList.length; i++)
+        if (e.target === playerList[i].button) {
+            playerList[i].score++;
+            playerList[i].display.innerText = playerList[i].score;
+        }
 })
